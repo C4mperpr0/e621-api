d665083ee84716c851da1870b907760328d170e9
C4mperpr0 <Carlbellgardt@gmail.com>
2023-11-27 04:31:47 +0100

    added reverse search by filename, still WIP

--- Api.py
+++ Api.py
@@ -12,16 +12,21 @@ class Api:
             f"{credentials['username']}:{credentials['api-key']}".encode('utf-8')).decode('utf-8')
         self.__session.headers.update({"Authorization": f"Basic {self.__auth_b64_string}",
                           "User-Agent": "e621-api/1.0.0 (by Furglacia on e621)"})
+        # self.e621_urls = {
+        #     "posts": "https://e621.net/posts.json",
+        #     "favorite_post": "https://e621.net/favorites.json"
+        # }
 
-    status_codes = {
+    __status_codes = {
       "200": "OK - Request was successful",
+      "201": "Success",
       "204": "No Content - Request was successful, nothing will be returned. Most often encountered when deleting a record",
       "403": "Forbidden - Access denied. May indicate that your request lacks a User-Agent header (see Notice #2 above)",
       "404": "Not Found - Not found",
       "412": "Precondition failed",
       "420": "Invalid Record - Record could not be saved",
       "421": "User Throttled - User is throttled, try again later",
-      "422": "Locked - The resource is locked and cannot be modified",
+      "422": "Locked - The resource is locked and cannot be modified / Unprocessable Entity - Post already favorited",
       "423": "Already Exists - Resource already exists",
       "424": "Invalid Parameters - The given parameters were invalid",
       "500": "Internal Server Error - Some unknown error occurred on the server",
@@ -34,11 +39,12 @@ class Api:
     }
 
     def _eval_http_status(response, is_json: bool = True, throttle: bool = True):
-        if not response.ok:
-            print(f"[{response.status_code}] \"{response.url}\": {Api.status_codes[f'{response.status_code}']}")
+        if False: #not response.ok: # TODO: remove later and use raise instead !!!!!!!!!!
+            print(f"[{response.status_code}] \"{response.url}\": {Api.__status_codes[f'{response.status_code}']}")
             quit()
         else:
-            sleep(.55)  # sleep to not go over the speed limit (2r/s)
+            if throttle:
+                sleep(.55)  # sleep to not go over the speed limit (2r/s)
             print(f"[{response.status_code}] \"{response.url}\"")
             return json.loads(response.content) if is_json else response.content
 
@@ -49,9 +55,9 @@ class Api:
                   query: str = "",
                   base_url: str = "https://e621.net/posts.json",
                   download: bool = False):
+        posts = []
         if fetch_all:
             dl_index = 0
-            posts = []
             last_amount = -1
             while len(posts) != last_amount:
                 last_amount = len(posts)
@@ -64,13 +70,21 @@ class Api:
                         self.download_post(posts[dl_index], quality=2)
                         dl_index += 1
                 page += 1
-            return posts
         else:
             position = f"limit={limit}&page={page}"
-            return Api._eval_http_status(self.__session.get(f"https://e621.net/posts?{position}&{query}"))
+            posts += [Post(post) for post in Api._eval_http_status(self.__session.get(
+                    f"{base_url}?{position}{'&' if query != '' else ''}{query}"))['posts']]
+        return posts
 
-    def get_favorites(self, fetch_all: bool = False, page: int = 1, limit: int = 250, query: str = ""):
-        return self.get_posts(fetch_all=fetch_all, page=page, limit=limit, query=query, base_url="https://e621.net/favorites.json")
+    def get_post_by_filename(self, filename: str):
+        post = self.get_posts(query=f"tags=md5:{filename.split('.')[0]}")
+        return None if len(post) != 1 else post[0]
+
+    def get_favorites(self, fetch_all: bool = False, page: int = 1, limit: int = 250, query: str = "", download: bool = False):
+        return self.get_posts(fetch_all=fetch_all, page=page, limit=limit, query=query, base_url="https://e621.net/favorites.json", download=download)
+
+    def add_favorite(self, post: Post):
+        Api._eval_http_status(self.__session.post("https://e621.net/favorites.json", data=f"post_id={post.id}"))
 
     def download_post(self, post: Post, quality: int=0):  # quality: 0-preview 1-sample 2-full
         url = post.file['url'] if quality == 2 else post.sample['url'] if quality == 1 else post.preview['url']
--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import json
 from Api import Api
+import os
 
 with open("credentials.json") as file:
     credentials = json.loads(file.read())
 
 api = Api(credentials)
-r = api.get_favorites(fetch_all=True)
-print(f"Final amount: {len(r)}")
+api.get_favorites(fetch_all=True, download=True)
 
-tmp = [api.download_post(post, quality=2) for post in r]
+'''
+for f in os.listdir("./Downloads/various stuff merge later/"):
+    if f.split('.')[1] == "webm" or f.split('.')[1] == "png" or f.split('.')[1] == "mp4":
+        if f.startswith("2021") or f.startswith("RDT"):
+            continue
+        f = f.removeprefix("sample_")
+        f = f.split(".")[0]
+        f = f.removesuffix("_480p")
+        print(f)
+        p = api.get_post_by_filename(f)
+        if p != None:
+            api.add_favorite(p)
 
+
+all_favs = api.get_favorites(fetch_all=True)
+for fav in all_favs:
+    print(f"{fav.file['md5']}.{fav.file['ext']}")
+    if f"{fav.file['md5']}.{fav.file['ext']}" in os.listdir("./Downloads/various stuff merge later/"):
+        os.remove(f"./Downloads/various stuff merge later/{fav.file['md5']}.{fav.file['ext']}")
+        print(f"Removed: {fav.file['md5']}.{fav.file['ext']}")
+    if f"sample_{fav.file['md5']}.{fav.file['ext']}" in os.listdir("./Downloads/various stuff merge later/"):
+        os.remove(f"./Downloads/various stuff merge later/sample_{fav.file['md5']}.{fav.file['ext']}")
+        print(f"Removed: sample_{fav.file['md5']}.{fav.file['ext']}")
+    if f"{fav.file['md5']}_480p.{fav.file['ext']}" in os.listdir("./Downloads/various stuff merge later/"):
+        os.remove(f"./Downloads/various stuff merge later/{fav.file['md5']}_480p.{fav.file['ext']}")
+        print(f"Removed: {fav.file['md5']}_480p.{fav.file['ext']}")
+'''
 print("done")
 
